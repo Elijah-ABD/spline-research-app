5b0da3c849b6cf9e7368aeb83b12dda1b28ca4ce
Elijah Afriyie-Olokun <Elijah.Afriyie-Olokun@abdynamics.com>
2024-07-31 12:07:33 +0100

    Finished chordal and centripetal CR

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using Accessibility;
+using System.Drawing;
 using System.Security.Cryptography.Pkcs;
 using System.Text;
 using System.Transactions;
@@ -10,17 +11,23 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MathNet.Numerics;
 
 
 namespace WpfApp1
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
 
-        private List<(double x, double y)> coords = new List<(double x, double y)>();
-        private List<(double x, double y)> splineCoords = new List<(double x, double y)>();
+        private List<Coord> coords = new List<Coord>();
+        private List<Coord> splineCoords = new List<Coord>();
+        struct Coord
+        {
+            public double x, y;
+        }
         public MainWindow()
         {
             InitializeComponent();
@@ -36,7 +43,8 @@ namespace WpfApp1
         private void MouseClickHandler(object sender, MouseEventArgs e)
         {
             var point = e.GetPosition(this);
-            coords.Add((point.X, point.Y));
+            Coord p;
+            coords.Add(new Coord { x = point.X, y = point.Y });
 
             Ellipse marker = new Ellipse
             {
@@ -57,6 +65,7 @@ namespace WpfApp1
             if ((e.Key == Key.Return) || (e.Key == Key.Space))
             {
                 DrawSpline();
+                SplineDrawing();
             }
 
             if (e.Key == Key.R)
@@ -88,70 +97,62 @@ namespace WpfApp1
 
         private void DrawSpline()
         {
-            for (double i = 0; i < coords.Count; i+=0.001)
+            for (int i = 0; i < coords.Count - 3; i++)
             {
                 //splineCoords.Add(coords[i]);
- 
-                    GetSplineCoord(i);
+                for (double t = 0; t < 1; t += 0.001)
+                {
+                    var point = GetSplineCoord(t, coords[i], coords[i + 1], coords[i + 2], coords[i + 3]);
+                    Ellipse marker = new Ellipse
+                    {
+                        Height = 2,
+                        Width = 2,
+                        Fill = Brushes.Blue,
+                    };
+                    Canvas.SetLeft(marker, point.x - marker.Width / 2);
+                    Canvas.SetTop(marker, point.y - marker.Height / 2);
+                    canvas.Children.Add(marker);
+                }
             }
 
         }
-        private void GetSplineCoord(double t, bool loop=false)
+        private Coord GetSplineCoord(double t, Coord p0, Coord p1, Coord p2, Coord p3, bool loop = false)
         {
-            int p0, p1, p2, p3;
+            var point = new Coord();
+            var t2 = t * t;
+            var t3 = t2 * t;
 
-            if (!loop)
-            {
-                p1 = (int)t + 1;
-                p2 = p1 + 1;
-                p3 = p2 + 1;
-                p0 = p1 - 1;
-            }
-            else
-            {
-                p1 = (int)t;
-                p2 = (p1 + 1) % coords.Count;
-                p3 = (p2 + 1) % coords.Count;
-                p0 = p1 > 1 ? p1 - 1 : coords.Count - 1;
-            }
 
-            t = t - (int)t;
+            point.x = 0.5f * ((2.0f * p1.x)
+                + (-p0.x + p2.x) * t
+                + (2.0f * p0.x - 5.0f * p1.x
+                + 4 * p2.x - p3.x) * t2 +
+                (-p0.x + 3.0f * p1.x - 3.0f * p2.x + p3.x) * t3);
 
-            double tSq = t * t;
-            double tCu = tSq * t;
+            point.y = 0.5f * ((2.0f * p1.y)
+                + (-p0.y + p2.y) * t
+                + (2.0f * p0.y - 5.0f * p1.y + 4 * p2.y - p3.y) * t2
+                + (-p0.y + 3.0f * p1.y - 3.0f * p2.y + p3.y) * t3);
 
-            double q0, q1, q2, q3;
 
-            q0 = -tCu + 2 * tSq - t;
-            q1 = 3 * tCu - 5 * tSq + 2;
-            q2 = -3 * tCu + 4 * tSq + t;
-            q3 = tCu - tSq;
+            return point;
 
-            double tX = 0.5 * (coords[p0].Item1 * q0 + coords[p1].Item1 * q1 + coords[p2].Item1 * q2 + coords[p3].Item1 * q3);
-            double tY = 0.5 * (coords[p0].Item2 * q0 + coords[p1].Item2 * q1 + coords[p2].Item2 * q2 + coords[p3].Item2 * q3);
-
-            Ellipse marker = new Ellipse
-            {
-                Height = 2,
-                Width = 2,
-                Fill = Brushes.Blue,
-            };
-            Canvas.SetLeft(marker, tX - marker.Width / 2);
-            Canvas.SetTop(marker, tY - marker.Height / 2);
-            canvas.Children.Add(marker);
         }
 
-        private (double,double) getDouble(string s){
+        private (double, double) getDouble(string s)
+        {
             var strings = s.Split(',');
             double x = Convert.ToDouble(strings[0]);
             double y = Convert.ToDouble(strings[1]);
             return (x, y);
-        } 
+        }
 
-        private void clearCanvas(){
-            while (canvas.Children.Count > 2){
-               canvas.Children.RemoveAt(canvas.Children.Count - 1);
-               
+        private void clearCanvas()
+        {
+            while (canvas.Children.Count > 2)
+            {
+                canvas.Children.RemoveAt(canvas.Children.Count - 1);
+
             }
         }
 
@@ -159,17 +160,93 @@ namespace WpfApp1
         {
             if (e.Key == Key.Enter)
             {
-            foreach (UIElement element in sp1.Children)
-            {
-                if (element is TextBox tb)
+                foreach (UIElement element in sp1.Children)
                 {
-                    coords.Add(getDouble(tb.Text));
+                    if (element is TextBox tb)
+                    {
+                        coords.Add(new Coord { x = getDouble(tb.Text).Item1, y = getDouble(tb.Text).Item2 });
+                    }
                 }
-            }
-            clearCanvas();
-            DrawSpline();
-            coords.Clear();
+                clearCanvas();
+                DrawSpline();
+                coords.Clear();
             }
         }
+        private void SplineDrawing()
+        {
+            var centripetalPoints = CRChain(coords, 1);
+            DrawPoints(coords, 10, Brushes.Black);
+            DrawPoints(centripetalPoints, 2, Brushes.Red);
+            
+        }
+
+        private List<Coord> CRChain(List<Coord> points, float alpha)
+        {
+            var chainedCoords = new List<Coord>();
+            for (int i = 0; i < points.Count - 3; i++)
+            {
+                chainedCoords.AddRange(CRPoint(points[i], points[i + 1], points[i + 2], points[i + 3], alpha));
+            }
+            return chainedCoords;
+        }
+
+        private double Distance(double ti, Coord pi, Coord pj, float alpha)
+        {
+            double xi = pi.x; double yi = pi.y;
+            double xj = pj.x; double yj = pj.y;
+            double dx = xj - xi; double dy = yj - yi;
+            double l = Math.Sqrt(dx * dx + dy * dy);
+            return ti + Math.Pow(l, alpha);
+        }
+
+        private List<Coord> CRPoint(Coord p0, Coord p1, Coord p2, Coord p3, float alpha)
+        {
+            double t0 = 0;
+            double t1 = Distance(t0, p0, p1, alpha);
+            double t2 = Distance(t1, p1, p2, alpha);
+            double t3 = Distance(t2, p2, p3, alpha);
+            double[] t = Generate.LinearSpaced(1000, t1, t2);
+            var points = new List<Coord>();
+            for (int i = 0; i < t.Length; i++)
+            {
+                var a1x = (t1 - t[i]) / (t1 - t0) * p0.x + (t[i] - t0) / (t1 - t0) * p1.x;
+                var a1y = (t1 - t[i]) / (t1 - t0) * p0.y + (t[i] - t0) / (t1 - t0) * p1.y;
+
+                var a2x = (t2 - t[i]) / (t2 - t1) * p1.x + (t[i] - t1) / (t2 - t1) * p2.x;
+                var a2y = (t2 - t[i]) / (t2 - t1) * p1.y + (t[i] - t1) / (t2 - t1) * p2.y;
+
+                var a3x = (t3 - t[i]) / (t3 - t2) * p2.x + (t[i] - t2) / (t3 - t2) * p3.x;
+                var a3y = (t3 - t[i]) / (t3 - t2) * p2.y + (t[i] - t2) / (t3 - t2) * p3.y;
+
+                var b1x = (t2 - t[i]) / (t2 - t0) * a1x + (t[i] - t0) / (t2 - t0) * a2x;
+                var b1y = (t2 - t[i]) / (t2 - t0) * a1y + (t[i] - t0) / (t2 - t0) * a2y;
+
+                var b2x = (t3 - t[i]) / (t3 - t1) * a2x + (t[i] - t1) / (t3 - t1) * a3x;
+                var b2y = (t3 - t[i]) / (t3 - t1) * a2y + (t[i] - t1) / (t3 - t1) * a3y;
+
+                var pointX = (t2 - t[i]) / (t2 - t1) * b1x + (t[i] - t1) / (t2 - t1) * b2x;
+                var pointY = (t2 - t[i]) / (t2 - t1) * b1y + (t[i] - t1) / (t2 - t1) * b2y;
+                points.Add(new Coord { x = pointX, y = pointY });
+            }
+            return points;
+        }
+
+        private void DrawPoints(List<Coord> points,int size, Brush colour) 
+        {
+            foreach (var point in points)
+            {
+                 Ellipse marker = new Ellipse
+                {
+                    Height = size,
+                    Width = size,
+                    Fill = colour,
+                };
+                Canvas.SetLeft(marker, point.x - marker.Width / 2);
+                Canvas.SetTop(marker, point.y - marker.Height / 2);
+                canvas.Children.Add(marker);
+            }
+
+        }
+
     }
 }
--- WpfApp1/Untitled-1.md
+++ WpfApp1/Untitled-1.md
@@ -24,22 +24,3 @@ The Chordal Catmull-Rom spline matrix is another variant of the Catmull-Rom spli
 | 0 0 1/2 1/2 |
 ```
 This matrix is used to compute the control points for a Chordal Catmull-Rom spline, which is a variant of the Catmull-Rom spline that has a more uniform parameterization based on
-
-q0 = -0.5 * tCu + tSq - 0.5 * t;
-q1 = 1.5 * tCu - 2.5 * tSq + 1;
-q2 = -1.5 * tCu + 2 * tSq + 0.5 * t;
-q3 = 0.5 * tCu - 0.5 * tSq;
-double tX = coords[p0].Item1 * q0 + coords[p1].Item1 * q1 + coords[p2].Item1 * q2 + coords[p3].Item1 * q3;
-double tY = coords[p0].Item2 * q0 + coords[p1].Item2 * q1 + coords[p2].Item2 * q2 + coords[p3].Item2 * q3;
-// ... (rest of the method remains the same)
-
-// Centripetal coefficients
-double alpha = 0.5; // adjust this value to change the shape of the curve
-double t2 = t * t;
-double t3 = t2 * t;
-q0 = (1 - alpha) * (1 - t3) + alpha * (1 - 3 * t2 + 2 * t3);
-q1 = (1 - alpha) * (3 * t - 2 * t2 - t3) + alpha * (4 * t - 4 * t2 + t3);
-q2 = (1 - alpha) * (t2 - t3) + alpha * (t2 - t3);
-q3 = (1 - alpha) * (t3) + alpha * (t3);
-double tX = coords[p0].Item1 * q0 + coords[p1].Item1 * q1 + coords[p2].Item1 * q2 + coords[p3].Item1 * q3;
-double tY = coords[p0].Item2 * q0 + coords[p1].Item2 * q1 + coords[p2].Item2 * q2 + coords[p3].Item2 * q3;
